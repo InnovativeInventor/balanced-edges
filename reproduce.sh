d8071cf3a9892243806f74ce26caeadcc7efcc0d
Max Fan <root@max.fan>
2022-06-03 14:48:40 -0400

    Add timeout to reproduce script

--- reproduce.sh
+++ reproduce.sh
@@ -1,1 +1,2 @@
-fd connected.json -j 1 -x bash -c 'balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'
+# fd vtd-connected.json -j 1 -x bash -c 'balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'
+fd vtd-connected.json -j 1 -x bash -c 'timeout 60 balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'

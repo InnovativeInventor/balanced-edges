b22f11fcccbf917a858bd6ed451c10379895730b
Max Fan <root@max.fan>
2022-06-04 14:14:44 -0400

    Add full path to balanced-edges executable

--- reproduce.sh
+++ reproduce.sh
@@ -1,3 +1,3 @@
 # fd vtd-connected.json dual-graphs -j 1 -x bash -c 'balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'
-fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 70 balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'
-fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.01 > logs/{/}.senate-1.log'
+fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'
+fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.01 > logs/{/}.senate-1.log'

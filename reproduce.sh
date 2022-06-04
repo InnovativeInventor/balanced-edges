b31fc3f4f748ca1c6493e93695b60d0ae252ddd5
Max Fan <root@max.fan>
2022-06-04 19:35:31 -0400

    Add cong calcs

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,6 @@
 # fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.03 > logs/{/}.senate-3.log'
 # fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber senate) -t 0.01 > logs/{/}.senate-1.log'
-fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber house) -t 0.03 > logs/{/}.house-3.log'
-fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber house) -t 0.01 > logs/{/}.house-1.log'
+# fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber house) -t 0.03 > logs/{/}.house-3.log'
+# fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 100 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber house) -t 0.01 > logs/{/}.house-1.log'
+fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 600 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber congress) -t 0.01 > logs/{/}.congress-1.log'
+fd vtd-connected.json dual-graphs -j 1 -x bash -c 'timeout 600 ./target/release/balanced-edges -f {} -d $(python python/seats_lookup.py {} --chamber congress) -t 0.03 > logs/{/}.congress-3.log'
